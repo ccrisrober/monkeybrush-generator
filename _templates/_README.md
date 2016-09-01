a8cc204655e7b80dc13402211a4b7a9176b8ef02
ccrisrober <ccrisrober@gmail.com>
2016-09-01 13:07:32 +0200

    Updated code

--- _templates/_README.md
+++ _templates/_README.md
@@ -1,5 +1,5 @@
 # {{name}}
-{{ description }}
+{{description}}
 
 ## Usage
 [![NPM](https://nodei.co/npm/{{name}}.png)](https://www.npmjs.com/package/{{name}})
--- index.js
+++ index.js
@@ -4,46 +4,66 @@ var readdirp = require('readdirp');
 var path = require("path");
 var fs = require("fs");
 var dotty = require("dotty");
-
-// https://github.com/SBoudrias/Inquirer.js/#examples
+var inquirer = require('inquirer');
 
 var target = process.cwd() + "/test";
 
 var config = {
 	name: "MonkeyBrush example",
+	description: "",
 	user: {
 		name: "foouser",
 		github: "myproject"
 	}
 };
 
-readdirp({
-	root: path.join(__dirname, "_templates")
-}).on("data", function(file) {
-	var dest = path.resolve(target, file.path);
-	if (!fs.existsSync(target)){
-    	fs.mkdirSync(target);
-	}
-
-	fs.readFile(file.fullPath, "utf-8", function(err, content) {
-		if (err) throw err;
-
-		content = render(content, config);
-
-		//console.log(content);
-
-		var n = dest.lastIndexOf("_");
-
-		// slice the string in 2, one from the start to the lastIndexOf
-		// and then replace the word in the rest
-		var str = dest.slice(0, n) + dest.slice(n).replace("_", "");
-
-		fs.writeFile(str, content)
-	});
-});
-
 function render(template, params) {
 	return template.replace(/\{\{([^}]+)}}/g, function(_, name) {
 		return dotty.get(params, name)
 	});
+};
+
+inquirer.prompt([{
+	type: "input",
+	name: "name",
+	message: "Project name",
+	default: path.basename(target)
+}, {
+	type: "input",
+	name: "description",
+	message: "Project description",
+	validate: function (str) {
+		return str.length >= 3;
+	}
 }
+]).then(function(responses) {
+	console.log(responses);
+	config.name = responses.name;
+	config.description = responses.description;
+	console.log(config);
+
+	readdirp({
+		root: path.join(__dirname, "_templates")
+	}).on("data", function(file) {
+		var dest = path.resolve(target, file.path);
+		if (!fs.existsSync(target)){
+	    	fs.mkdirSync(target);
+		}
+
+		fs.readFile(file.fullPath, "utf-8", function(err, content) {
+			if (err) throw err;
+
+			content = render(content, config);
+
+			//console.log(content);
+
+			var n = dest.lastIndexOf("_");
+
+			// slice the string in 2, one from the start to the lastIndexOf
+			// and then replace the word in the rest
+			var str = dest.slice(0, n) + dest.slice(n).replace("_", "");
+
+			fs.writeFile(str, content)
+		});
+	});
+});
